4aa57755dd9a9ad31876954d198b84e75aaacf91
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-06-11 17:46:44 +0900

    途中経過

--- polls/urls.py
+++ polls/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
   path('', views.index, name='index'),
+  path('submit', views.submit_order, name='submit_order'),
+  path('login', views.login, name='login'),
+  path('confirm', views.confirm, name='confirm'),
+  path('detail', views.detail, name='detail'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -6,5 +6,16 @@ from django.urls import reverse
 from django.views import generic
 
 def index(request):
-  return render(request, 'polls/start_view.html')
+  return render(request, 'polls/menu1.html')
 
+def submit_order(request):
+  return render(request, 'polls/set_order_view.html')
+
+def login(request):
+  return render(request, 'polls/login_view.html')
+
+def confirm(request):
+  return render(request, 'polls/confirm_order_view.html')
+
+def detail(request):
+  return render(request, 'polls/order_detail.html')
