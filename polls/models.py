2532f4283a81be084cccc3c1771362cac7e76687
Osadakun <b1915053@planet.kanazawa-it.ac.jp>
2022-06-11 19:09:57 +0900

    途中経過

--- polls/models.py
+++ polls/models.py
@@ -4,19 +4,21 @@ from django.db import models
 from django.utils import timezone
 
 class Team(models.Model):
+  syumoku = models.CharField(max_length=10)
   team_name = models.CharField(max_length=20)
-  member1 = models.CharField(max_length=20)
-  member2 = models.CharField(max_length=20)
-  member3 = models.CharField(max_length=20)
-  member4 = models.CharField(max_length=20)
-  member5 = models.CharField(max_length=20)
-  member6 = models.CharField(max_length=20)
-  member7 = models.CharField(max_length=20)
-  member8 = models.CharField(max_length=20)
-  member9 = models.CharField(max_length=20)
-  member10 = models.CharField(max_length=20)
+  member1 = models.CharField(max_length=20, null=True)
+  member2 = models.CharField(max_length=20, null=True)
+  member3 = models.CharField(max_length=20, null=True)
+  member4 = models.CharField(max_length=20, null=True)
+  member5 = models.CharField(max_length=20, null=True)
+  member6 = models.CharField(max_length=20, null=True)
+  member7 = models.CharField(max_length=20, null=True)
+  member8 = models.CharField(max_length=20, null=True)
+  member9 = models.CharField(max_length=20, null=True)
+  member10 = models.CharField(max_length=20, null=True)
 
 class Order(models.Model):
+  syumoku = models.CharField(max_length=10)
   team_name = models.CharField(max_length=20)
   enemy_name = models.CharField(max_length=20)
   single_6 = models.CharField(max_length=20)
